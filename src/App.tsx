53c1d91cdd4f7a604a852b3b44a7769f62b062d3
Sergei Babin <sergei.babin@ingka.ikea.com>
2025-05-30 00:08:01 +0200

    16

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import AboutPage from './pages/AboutPage';
-import LandingPage from './pages/LandingPage';
 import AssessmentPage from './pages/AssessmentPage';
+import LandingPage from './pages/LandingPage';
 import ResultsPage from './pages/ResultsPage';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="bg-gray-50 font-sans">
-      <NavBar />
-      <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/assessment" element={<AssessmentPage />} />
-          <Route path="/results" element={<ResultsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter basename="/communication-styles/">
+      <div className="bg-gray-50 font-sans">
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/assessment" element={<AssessmentPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
